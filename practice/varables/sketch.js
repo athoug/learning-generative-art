1126628c50c6522775083c8ad8f944937f15f309
Athoug Alsoughayer <athoug.s@gmail.com>
2017-03-24 07:58:38 -0400

    playing with variables

---
+++ practice/varables/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(600,400);
+  // background
+  background(250,250,100,200);
+}
+
+function draw() {
+  
+  
+  // ellipse
+  fill(250,200,200, 100);
+  noStroke();
+  ellipse(mouseX,mouseY,100,100);
+  
+  // rectangle
+  // fill(200,250,200);
+  // rect(400,100,50,50);
+}
+
+function mousePressed(){
+  // background
+  background(250,250,100, 200);
+}
