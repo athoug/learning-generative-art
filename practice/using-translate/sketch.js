3b1f5e4032da918cd15c88f1d61c701c08dd556a
Athoug Alsoughayer <athoug.s@gmail.com>
2017-04-04 10:43:14 -0400

    more translate practice

---
+++ practice/using-translate/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(240);
+  translate(160,120);
+  drawGrid();
+  fill(0);
+  rect(60,60,100,100);
+}
+
+function drawGrid(){
+  stroke(200);
+  fill(120);
+  
+  for(var x = -width; x < width; x +=40){
+    line(x, -height, x, height);
+    text(x, x+1, 12);
+  }
+  
+  for(var y = -height; y < height; y +=40){
+    line(-width, y, width, y);
+    text(y, 1, y+12);
+  }
+}
