0a6de4b341c19992cf9671a99f96142438f8f7bf
Athoug Alsoughayer <athoug2.s@gmail.com>
2018-01-07 07:22:12 +0300

    change colors

--- 2018/p5 Sketches/animated-white-lines/sketch.js
+++ 2018/p5 Sketches/animated-white-lines/sketch.js
@@ -2,7 +2,8 @@ function setup() {
   createCanvas(window.innerWidth, window.innerHeight); // set up the size of the window
 }
 function draw() {
-    background(20); // setup a background colorod a shade of black
-    stroke(250); // set up a white shade for the lines to be drawn
+    background(142, 68, 173); // setup a background colorod a shade of purple
+    stroke(155, 89, 182); // set up a light purple shade for the lines to be drawn
+    strokeWeight(10); // increase the line width
     line(0, random(height), width, random(height));
 }
